2e92a7db0515a103fc502bb56cec930cbad5a471
ryanSacatani <sacataniryan1@gmail.com>
2021-07-18 14:50:50 +0800

    bug fixing

--- zoobar/rpclib.py
+++ zoobar/rpclib.py
@@ -92,7 +92,7 @@ class RpcClient(object):
         self.close()
 
 def client_connect(pathname):
-    debug.log("AAAAAAAAAAAAAAAA" + pathname)
+    log("AAAAAAAAAAAAAAAA" + pathname)
     sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
     sock.connect(pathname)
     return RpcClient(sock)
